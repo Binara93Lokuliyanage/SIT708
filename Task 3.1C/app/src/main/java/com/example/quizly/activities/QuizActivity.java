e75c3bf8e5af66d6a52b7fb95f9af0441fb5d08a
Piyu HP Binu <mr.binara@gmail.com>
2026-04-09 22:05:04 +1000

    finalised 3.1C

--- Task 3.1C/app/src/main/java/com/example/quizly/activities/QuizActivity.java
+++ Task 3.1C/app/src/main/java/com/example/quizly/activities/QuizActivity.java
@@ -8,16 +8,17 @@ import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.content.ContextCompat;
 
 import com.example.quizly.R;
 import com.example.quizly.models.Question;
 import com.example.quizly.utils.QuizData;
 
 public class QuizActivity extends AppCompatActivity {
+
     TextView questionText;
     Button option1, option2, option3, option4, submitBtn;
     ProgressBar progressBar;
+    String username;
 
     Question[] questions;
 
@@ -25,6 +26,7 @@ public class QuizActivity extends AppCompatActivity {
     int selectedOption = -1;
     int score = 0;
 
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz);
@@ -37,34 +39,16 @@ public class QuizActivity extends AppCompatActivity {
         submitBtn = findViewById(R.id.submitBtn);
         progressBar = findViewById(R.id.progressBar);
 
+        username = getIntent().getStringExtra("username");
+
         questions = QuizData.getQuestions();
 
         loadQuestion();
 
-        option1.setOnClickListener(v -> {
-            selectedOption = 0;
-            option1.setBackgroundTintList(
-                    ContextCompat.getColorStateList(this, R.color.option_selected)
-            );
-        });
-        option2.setOnClickListener(v -> {
-            selectedOption = 1;
-            option2.setBackgroundTintList(
-                    ContextCompat.getColorStateList(this, R.color.option_selected)
-            );
-        });
-        option3.setOnClickListener(v -> {
-            selectedOption = 2;
-            option3.setBackgroundTintList(
-                    ContextCompat.getColorStateList(this, R.color.option_selected)
-            );
-        });
-        option4.setOnClickListener(v -> {
-            selectedOption = 3;
-            option4.setBackgroundTintList(
-                    ContextCompat.getColorStateList(this, R.color.option_selected)
-            );
-        });
+        option1.setOnClickListener(v -> selectedOption = 0);
+        option2.setOnClickListener(v -> selectedOption = 1);
+        option3.setOnClickListener(v -> selectedOption = 2);
+        option4.setOnClickListener(v -> selectedOption = 3);
 
         submitBtn.setOnClickListener(v -> handleSubmit());
     }
@@ -96,11 +80,11 @@ public class QuizActivity extends AppCompatActivity {
         for (int i = 0; i < options.length; i++) {
 
             if (i == correctIndex) {
-                options[i].setBackgroundColor(R.color.option_correct);
-                options[i].setTextColor(R.color.black);
+                options[i].setBackgroundColor(Color.GREEN);
+                options[i].setTextColor(Color.WHITE);
             } else if (i == selectedOption) {
                 options[i].setBackgroundColor(Color.RED);
-                options[i].setTextColor(R.color.white);
+                options[i].setTextColor(Color.WHITE);
             }
 
             options[i].setEnabled(false);
@@ -125,6 +109,7 @@ public class QuizActivity extends AppCompatActivity {
         } else {
             Intent intent = new Intent(QuizActivity.this, ResultActivity.class);
             intent.putExtra("score", score);
+            intent.putExtra("username", username);
             startActivity(intent);
             finish();
         }
@@ -135,7 +120,8 @@ public class QuizActivity extends AppCompatActivity {
 
         for (Button btn : options) {
             btn.setEnabled(true);
-            btn.setBackgroundColor(getColor(R.color.option_default));
+            btn.setBackgroundColor(getColor(android.R.color.darker_gray));
+            btn.setTextColor(getColor(android.R.color.black));
         }
     }
 
@@ -143,6 +129,4 @@ public class QuizActivity extends AppCompatActivity {
         int progress = (int) (((currentQuestionIndex + 1) * 100.0f) / questions.length);
         progressBar.setProgress(progress);
     }
-
-
 }
